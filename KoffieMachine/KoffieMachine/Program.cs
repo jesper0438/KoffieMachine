e631cc632559a502f72257b8fd3af46cdec21a41
jesper0438 <robr0002@hz.nl>
2017-11-16 11:11:17 +0100

    fixed resources

--- KoffieMachine/KoffieMachine/Program.cs
+++ KoffieMachine/KoffieMachine/Program.cs
@@ -6,31 +6,43 @@ namespace KoffieMachine
     {
         static void Main(string[] args)
         {
-            // maak de koffiemachine aan
-            KoffieMachine italiaansekoffiemachine = new KoffieMachine(20,20,20);
+           
+                // maak de koffiemachine aan
+                KoffieMachine italiaansekoffiemachine = new KoffieMachine(20, 20, 20);
+                Console.WriteLine("maak keuze");
 
-            //Bekertje koffiezwart = italiaansekoffiemachine.GeefKoffiezwart();
-          
-            ConsoleKeyInfo KeyInfo = Console.ReadKey();
+            while (true)
+            {
 
-            if (KeyInfo.KeyChar == '0')
+                //Bekertje koffiezwart = italiaansekoffiemachine.GeefKoffiezwart();
+
+                ConsoleKeyInfo KeyInfo = Console.ReadKey();
+
+                if (KeyInfo.KeyChar == '0')
                 {
-                Bekertje mijnEersteBakje = italiaansekoffiemachine.GeefCappuccino();
-                mijnEersteBakje.Print();
-            }
-            if (KeyInfo.KeyChar == '1')
-            {
-                Bekertje mijnEersteBakje = italiaansekoffiemachine.GeefKoffiezwart();
-                mijnEersteBakje.Print();
-            }
-            if (KeyInfo.KeyChar == '2')
-            {
-                Bekertje mijnEersteBakje = italiaansekoffiemachine.GeefLattemacchiato();
-                mijnEersteBakje.Print();
-            }
+                    Bekertje cappuccino = italiaansekoffiemachine.GeefCappuccino();
+                    cappuccino.Print();
+                    Console.WriteLine(italiaansekoffiemachine);
+                    Console.ReadLine();
+                }
+                if (KeyInfo.KeyChar == '1')
+                {
+                    Bekertje koffiezwart = italiaansekoffiemachine.GeefKoffiezwart();
+                    koffiezwart.Print();
+                    Console.WriteLine(italiaansekoffiemachine);
+                    Console.ReadLine();
+                }
+                if (KeyInfo.KeyChar == '2')
+                {
+                    Bekertje lattemacchiato = italiaansekoffiemachine.GeefLattemacchiato();
+                    lattemacchiato.Print();
+                    Console.WriteLine(italiaansekoffiemachine);
+                    Console.ReadLine();
+                }
 
-            Console.Read();
-            // Doe dit net zo vaak totdat er een exeption optreedt
+
+                // Doe dit net zo vaak totdat er een exeption optreedt
+            }
         }
     }
 }
